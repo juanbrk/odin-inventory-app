dad1f831734bd99f321df5c90095b9a2535a54b2
JuanG <juangiacosabrk@gmail.com>
2020-11-22 16:32:48 -0300

    Implemented Brand forms for creation and update

--- controllers/brandController.js
+++ controllers/brandController.js
@@ -3,6 +3,7 @@ var Item = require('../models/item');
 var Seed = require('../models/seed');
 
 var async = require('async');
+const { body,validationResult } = require("express-validator");
 
 
 // Display list of all Brands.
@@ -47,13 +48,55 @@ exports.brand_detail = function(req, res, next) {
 
 // Display Brand create form on GET.
 exports.brand_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Brand create GET');
+    res.render('brand_form', { title: 'Create New Brand' });
 };
 
 // Handle Brand create on POST.
-exports.brand_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Brand create POST');
-};
+exports.brand_create_post =  [
+   
+    // Validate and santise the name field.
+    body('name', 'Brand name required').trim().isLength({ min: 1 }).escape(),
+  
+    // Process request after validation and sanitization.
+    (req, res, next) => {
+  
+      // Extract the validation errors from a request.
+      const errors = validationResult(req);
+  
+      // Create a brand object with escaped and trimmed data.
+      var brand = new Brand(
+        { name: req.body.name }
+      );
+  
+  
+      if (!errors.isEmpty()) {
+        // There are errors. Render the form again with sanitized values/error messages.
+        res.render('brand_form', { title: 'Create Brand', brand: brand, errors: errors.array()});
+        return;
+      }
+      else {
+        // Data from form is valid.
+        // Check if Brand with same name already exists.
+        Brand.findOne({ 'name': req.body.name })
+          .exec( function(err, found_brand) {
+             if (err) { return next(err); }
+  
+             if (found_brand) {
+               // Brand exists, redirect to its detail page.
+               res.redirect(found_brand.url);
+             }
+             else {
+
+               brand.save(function (err) {
+                 if (err) { return next(err); }
+                 // Brand saved. Redirect to brand detail page.
+                 res.redirect(brand.url);
+               });
+             }  
+           });
+      }
+    }
+  ];
 
 // Display Brand delete form on GET.
 exports.brand_delete_get = function(req, res) {
