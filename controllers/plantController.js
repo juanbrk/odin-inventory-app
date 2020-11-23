2c9eb82d6e7f000b878bdc20faefd63b6e0561f8
JuanG <juangiacosabrk@gmail.com>
2020-11-23 12:05:45 -0300

    Implemented plant creation form Switched to import and display Plant instead of Item at plant_list and controller

--- controllers/plantController.js
+++ controllers/plantController.js
@@ -1,8 +1,10 @@
 var Plant = require('../models/plant');
 var Seed = require('../models/seed');
+var Item = require('../models/item');
 var PlantType = require('../models/plant_type');
 
 var async = require('async');
+const { body,validationResult } = require('express-validator');
 
 
 exports.index = function(req, res) {   
@@ -18,7 +20,7 @@ exports.index = function(req, res) {
             Seed.countDocuments({}, callback);
         },
         item_count: function(callback) {
-            Plant.countDocuments({}, callback);
+            Item.countDocuments({}, callback);
         }
 
     }, function(err, results) {
@@ -29,6 +31,7 @@ exports.index = function(req, res) {
 // Display list of all Plants.
 exports.plant_list = function(req, res) {
     Plant.find({}, 'name plant_type price status')
+    .populate('plant_type')
     .exec(function (err, plants) {
       if (err) { return next(err); }
       //Successful, so render
@@ -50,13 +53,68 @@ exports.plant_detail = function(req, res) {
 
 // Display Plant create form on GET.
 exports.plant_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Plant create GET');
+    // Get all  plant types which we can use for adding to our plant.
+    async.parallel({
+        plant_types: function(callback) {
+            PlantType.find({}, 'name', callback);
+        },
+    }, function(err, results) {
+        if (err) { return next(err); }
+        res.render('plant_form', { title: 'New Plant', plant_types: results.plant_types });
+    });
 };
 
 // Handle Plant create on POST.
-exports.plant_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Plant create POST');
-};
+exports.plant_create_post = [
+    // Validate and sanitise fields.
+    body('name', 'Name not specified').trim().isLength({ min: 1 }).escape(),
+    body('status', 'Status not selected').trim().isIn(['Available', 'Sold out', 'Coming soon','Unavailable']).isLength({ min: 1 }).escape(),
+    body('plant_type', 'Plant Type not selected').trim().isLength({ min: 1 }).escape(),
+    body('stock', 'Invalid stock').trim().optional().isNumeric().escape(),
+    body('date_of_sow','Invalid date of sow' ).optional({ checkFalsy: true }).isISO8601().toDate(),
+    body('price').isCurrency().isNumeric().optional().escape(),
+    body('family_name', 'Invalid family name').optional({ checkFalsy: true }).trim().isLength({ min: 1 }).escape(),
+    
+    // Process request after validation and sanitization.
+    (req, res, next) => {
+
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
+
+        // Create a Plant object with escaped and trimmed data.
+        var plant = new Plant(
+          { name: req.body.name,
+            status: req.body.status,
+            plant_type: req.body.plant_type,
+            stock: req.body.stock,
+            date_of_sow: req.body.date_of_sow,
+            price: req.body.price,
+            family_name: req.body.family_name
+           });
+
+        if (!errors.isEmpty()) {
+            // There are errors. Render form again with sanitized values and error messages.
+            async.parallel({
+                plant_types: function(callback) {
+                    PlantType.find({}, 'name', callback);
+                },
+            }, function (err, results) {
+                    if (err) { return next(err); }
+                    // Successful, so render.
+                    res.render('plant_form', { title: 'New Plant', plant_types: results.plant_types, errors: errors.array() });
+            });
+            return;
+        }
+        else {
+            // Data from form is valid.
+            plant.save(function (err) {
+                if (err) { return next(err); }
+                   // Successful - redirect to new record.
+                   res.redirect(plant.url);
+                });
+        }
+    }
+];
 
 // Display Plant delete form on GET.
 exports.plant_delete_get = function(req, res) {
